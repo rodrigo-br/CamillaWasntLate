95eee6a65ad288602e43eb8750a91e3e1f79a3f2
Rodrigo Alves  Brito <rodrigoab123@gmail.com>
2023-06-06 10:39:11 -0300

    trees

--- Assets/Scripts/Managers/ActivePlayerManager.cs
+++ Assets/Scripts/Managers/ActivePlayerManager.cs
@@ -7,11 +7,11 @@ using Cinemachine;
 public class ActivePlayerManager : MonoBehaviour
 {
     [SerializeField] private PlayerKinematic[] playersInScene;
-    [SerializeField] CinemachineStateDrivenCamera sdCamera;
+    [SerializeField] CinemachineVirtualCamera virtualCamera;
 
     void Start()
     {
-        sdCamera.Follow = playersInScene[0].transform;
+        virtualCamera.Follow = playersInScene[0].transform;
     }
 
     private void OnSelectPlayer(InputValue value)
@@ -19,7 +19,7 @@ public class ActivePlayerManager : MonoBehaviour
         int pressed = (int)value.Get<float>();
         if (pressed != 0 && pressed <= playersInScene.Length)
         {
-            sdCamera.Follow = playersInScene[pressed - 1].transform;
+            virtualCamera.Follow = playersInScene[pressed - 1].transform;
             foreach (PlayerKinematic player in playersInScene)
             {
                 player.SetSelectedPlayer(pressed);
