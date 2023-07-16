3a342f5764cbc7e7db6e7271b3a11123097ab146
Rodrigo Alves  Brito <rodrigoab123@gmail.com>
2023-07-16 18:52:33 -0300

    desfudeu

--- Assets/Scripts/ExitPortal.cs
+++ Assets/Scripts/ExitPortal.cs
@@ -27,7 +27,7 @@ public class ExitPortal : MonoBehaviour
 
     void OnTriggerExit2D(Collider2D other)
     {
-        PlayerMovements player = other.transform.parent.GetComponentInChildren<PlayerMovements>();
+        PlayerMovements player = other.gameObject.GetComponent<PlayerMovements>();
 
         if (player != null && player.Id == id)
         {
